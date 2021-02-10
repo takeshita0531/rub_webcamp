ceebe62ff34e7f80af2dbcb2f912bb09cf553ff8
EC2 Default User <ec2-user@ip-172-31-91-222.ec2.internal>
2021-02-10 08:33:42 +0000

    [Finish] 7-1 and 7-2

---
+++ input.rb
@@ -0,0 +1,3 @@
+puts "キーボードからなにか入力してみましょう"
+input_key = gets
+puts "入力された内容は#{input_key}"
---
+++ lesson7-1.rb
@@ -0,0 +1,9 @@
+puts "計算を始めめます"
+puts "2つの値を入力してください"
+
+first_number = gets.to_i
+second_number = gets.to_i
+puts "計算結果を出力します"
+total = first_number * second_number
+puts "#{first_number} * #{second_number} = #{total}"
+puts "計算を終了します"
